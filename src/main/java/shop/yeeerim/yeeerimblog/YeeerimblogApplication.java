183dcb004ef47aff4a6ec9fbacc1e31c9443f694
yeeeeerim <gyr3904@gmail.com>
2023-04-19 21:17:23 +0900

    inital commit

---
+++ src/main/java/shop/yeeerim/yeeerimblog/YeeerimblogApplication.java
@@ -0,0 +1,13 @@
+package shop.yeeerim.yeeerimblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeeerimblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeeerimblogApplication.class, args);
+	}
+
+}
