792b6e226ce5dde0ed121a3be038c5b70511c95d
yeeeeerim <gyr3904@gmail.com>
2023-04-19 21:36:12 +0900

    security setting

---
+++ src/main/java/shop/yeeerim/yeeerimblog/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package shop.yeeerim.yeeerimblog.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.weaver.BCException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+@Slf4j
+@Configuration
+public class SecurityConfig {
+
+	//password 인코딩
+	@Bean
+	BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	//권한주소 설정
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+		//1. CSRF 해제
+		http.csrf().disable();
+
+		//2.Form 로그인 설정
+		http.formLogin().loginPage("/loginForm")
+				.loginProcessingUrl("/login")
+				.successHandler((request, response, authentication) -> {
+					log.debug("디버그 : 로그인 성공");
+				})
+				.failureHandler((request, response, exception) -> {
+					log.debug("디버그 : 로그인 실패: "+ exception.getMessage());
+				});
+
+		//3. 인증 권한 필터 설정 (s로 시작하는 주소로 들어오면 무조건 인증처리 해야함)
+		http.authorizeRequests(
+				authorize ->authorize.antMatchers("/s/**").authenticated().anyRequest().permitAll()
+		);
+
+
+		return http.build();
+	}
+}
