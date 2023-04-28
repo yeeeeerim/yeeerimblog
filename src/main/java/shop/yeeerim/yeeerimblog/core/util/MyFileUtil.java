1550069b468105afe7be4c3a79b1bb51b7a985b5
yeeeeerim <gyr3904@gmail.com>
2023-04-28 20:04:40 +0900

    profile변경완료

---
+++ src/main/java/shop/yeeerim/yeeerimblog/core/util/MyFileUtil.java
@@ -0,0 +1,24 @@
+package shop.yeeerim.yeeerimblog.core.util;
+
+import org.springframework.web.multipart.MultipartFile;
+import shop.yeeerim.yeeerimblog.core.exception.ssr.Exception500;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+public class MyFileUtil {
+	public static String write(String uploadFolder, MultipartFile file) {
+		UUID uuid = UUID.randomUUID();
+		String originalFilename = file.getOriginalFilename();
+		String uuidFilename = uuid + "_" + originalFilename;
+		try {
+			Path filePath = Paths.get(uploadFolder + uuidFilename);
+			Files.write(filePath, file.getBytes());
+		} catch (Exception e) {
+			throw new Exception500("파일 업로드 실패 : "+e.getMessage());
+		}
+		return uuidFilename;
+	}
+}
--- src/main/java/shop/yeeerim/yeeerimblog/service/UserService.java
+++ src/main/java/shop/yeeerim/yeeerimblog/service/UserService.java
@@ -2,16 +2,24 @@ package shop.yeeerim.yeeerimblog.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import shop.yeeerim.yeeerimblog.core.exception.ssr.Exception400;
+import shop.yeeerim.yeeerimblog.core.exception.ssr.Exception500;
+import shop.yeeerim.yeeerimblog.core.util.MyFileUtil;
 import shop.yeeerim.yeeerimblog.dto.user.UserRequest;
+import shop.yeeerim.yeeerimblog.model.user.User;
 import shop.yeeerim.yeeerimblog.model.user.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+	@Value("${file.path}")
+	private String uploadFolder;
 	private final UserRepository userRepository;
 	private final BCryptPasswordEncoder passwordEncoder; //시큐리티에서 Bean등록함
 
@@ -28,4 +36,23 @@ public class UserService {
 		}
 
 	} // 더티체킹, DB 세션종료
+
+	public User 회원프로필보기(Long id) {
+		User userPS = userRepository.findById(id)
+				.orElseThrow(()->new Exception400("id", "해당 유저가 존재하지 않습니다"));
+		return userPS;
+	}
+
+	@Transactional
+	public User 프로필사진수정(MultipartFile profile, Long id) {
+		try {
+			String uuidImageName = MyFileUtil.write(uploadFolder, profile);
+			User userPS = userRepository.findById(id)
+					.orElseThrow(()->new Exception500("로그인 된 유저가 DB에 존재하지 않음"));
+			userPS.changeProfile(uuidImageName);
+			return userPS;
+		}catch (Exception e){
+			throw new Exception500("프로필 사진 등록 실패 : "+e.getMessage());
+		}
+	}
 }
